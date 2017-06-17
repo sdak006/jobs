02655865ca17c73ff63ce443351b9171ff0baa6c
Jade\Scott <sdak006@gmail.com>
2017-06-17 14:06:04 -0500

    added styling to review/settings navigation

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, StatusBar } from 'react-native';
 import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
   //used by navigator
-  static navigationOptions = ({navigation}) => ({
-    title: 'Review Jobs',
+  static navigationOptions = ({ navigation }) => ({
     //styling and customization for the header
-    headerRight: <Button 
-                    title='Settings' 
-                    onPress={() => { navigation.navigate('settings') }}
-                  />
-    })
-  
+    title: 'Review Jobs',
+    headerStyle: {
+        //If the app is running on Android assign 24 to marginTop, if not, assign 0 to marginTop
+        paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+    },
+    headerRight: (
+      <Button
+        title='Settings'
+        onPress={() => { navigation.navigate('settings') }}
+        backgroundColor='rgba(0,0,0,0)'
+        color='rgba(0,122,255,1)'
+      />
+    )
+  })
+
   render() {
     return (
       <View>
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 
 class SettingsScreeen extends Component {
+static navigationOptions = ({ navigation }) => ({
+    //styling and customization for the header
+    title: 'Settings',
+    headerStyle: {
+        //If the app is running on Android assign 24 to marginTop, if not, assign 0 to marginTop
+        marginTop: Platform.OS === 'android' ? 24 : 0
+    }
+  })
+
   render() {
     return (
       <View>
