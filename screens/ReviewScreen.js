709446130ebf7bad26c5f10e5f72b475f3d4a496
Jade\Scott <sdak006@gmail.com>
2017-06-17 13:41:44 -0500

    added settings navigation

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  //used by navigator
+  static navigationOptions = ({navigation}) => ({
+    title: 'Review Jobs',
+    //styling and customization for the header
+    headerRight: <Button 
+                    title='Settings' 
+                    onPress={() => { navigation.navigate('settings') }}
+                  />
+    })
+  
   render() {
     return (
       <View>
@@ -15,5 +26,6 @@ class ReviewScreen extends Component {
     );
   }
 }
+
 
 export default ReviewScreen;
