74c7602da90d01d729461a94ebc28bd2eabf7948
Jade\Scott <sdak006@gmail.com>
2017-06-18 10:53:06 -0500

    finished auth handling

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,6 +1,5 @@
-import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import { AppLoading } from 'expo';
 import Slides from '../components/Slides';
 
@@ -13,12 +12,22 @@ const SLIDE_DATA = [
 class WelcomeScreen extends Component {
   state = { token: null }
 
+  async componentWillMount() {
+    let token = await AsyncStorage.getItem('fb_token');
+
+    if (token) {
+      this.props.navigation.navigate('map');
+    } else {
+      this.setState({ token: false });
+    }
+  }
+
   onSlidesComplete = () => {
     this.props.navigation.navigate('auth');
   }
 
   render() {
-    if (_.isNull(this.state.token)) {
+    if (this.state.token === null) {
       return <AppLoading />;
     }
     return (
