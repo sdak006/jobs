1c1649bb49932ac5e49f10bf49b10edc126cc430
Jade\Scott <sdak006@gmail.com>
2017-06-21 18:09:40 -0500

    foo

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { REHYDRATE } from 'redux-persist/constants';
 import {
   LIKE_JOB,
   CLEAR_LIKED_JOBS
@@ -12,6 +13,8 @@ export default function(state = [], action) {
       ], 'jobkey');
     case CLEAR_LIKED_JOBS:
       return [];
+    case REHYDRATE:
+      return action.payload.likedJobs || [];
     default:
       return state;
   }
